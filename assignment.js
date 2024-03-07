bda4c6bebc96c1d5a8dae5b5e89af390feb56e65
Your Name <abdullahiraziah@gmail.com>
2024-03-07 08:22:42 +0300

    strings

---
+++ assignment.js
@@ -0,0 +1,29 @@
+let x = 'extravaganza';
+console.log('extravaganza',x[8]);
+console.log('extravaganza',x[9]);
+console.log('extravaganza',x[10]);
+console.log('extravaganza',x[11]);
+
+const food="The quick brown fox jumped over the lazy dog";
+let r = 'eat'
+console.log("The ${r} quick brown fox jumped over the lazy dog");
+
+let s="wonderful"
+console.log(s.toUpperCase ("wonderful"));
+
+let m="amazing"
+console.log(m.toLowerCase ("amazing"));
+let n="undERneath"
+console.log(n.toLowerCase ("undERneath"));
+
+const story="The quick brown fox jumps over the lazy dog";
+console.log((story.match(/The/g)).length);
+console.log((story. match(/brown/g)).length);
+
+const string1="The pupils are reading in the library"
+let v= string1.indexOf('are');
+console.log(string1.indexOf('are'));
+
+const string2="The child was sitting on the table before it fell";
+let w = string2.indexOf('are');
+console.log(string2.indexOf('are'));
